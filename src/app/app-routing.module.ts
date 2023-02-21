0e02afb3a9f5c8f7859c69eb5c49278d7eaa9db0
oscarmauriciogiraldo <oscardan@utp.edu.co>
2023-02-20 22:16:51 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "Porfolio-Oscar-Giraldo", component: HomeComponent},
+  {path: "Porfolio-Oscar-Giraldo", component: HomeComponent},
+  {path: "", redirectTo: "/Porfolio-Oscar-Giraldo", pathMatch: "full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
