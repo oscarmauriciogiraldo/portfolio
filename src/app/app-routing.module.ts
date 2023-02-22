d1ac183a48b6b8f18e1a778985806ceea1a5d60c
oscarmauriciogiraldo <oscardan@utp.edu.co>
2023-02-21 22:40:20 -0500

    landing - portfolio - homecomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CvComponent } from './pages/cv/cv.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path: "Porfolio-Oscar-Giraldo", component: HomeComponent},
-  {path: "Porfolio-Oscar-Giraldo", component: HomeComponent},
+  {path: "Curriculum-Vitae", component: CvComponent},
   {path: "", redirectTo: "/Porfolio-Oscar-Giraldo", pathMatch: "full"},
 ];
 
