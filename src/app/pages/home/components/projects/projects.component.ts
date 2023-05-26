306f0b871641f7a3232bdeec2cce0d2c99a913b5
oscar.giraldo <oscar.giraldo@codesa.com.co>
2023-05-26 17:17:19 -0500

    cards project component section

--- src/app/pages/home/components/projects/projects.component.ts
+++ src/app/pages/home/components/projects/projects.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as Aos from 'aos';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit{
 
   showDescription = false;
+
+  ngOnInit(): void {
+    Aos.init();
+  }
 
   toggleButton(){
     this.showDescription = !this.showDescription;
