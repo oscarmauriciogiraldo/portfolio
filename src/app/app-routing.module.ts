30077160a88f0f3f2e8f68319d99fc6e336188ca
oscar.giraldo <oscar.giraldo@codesa.com.co>
2023-06-01 17:42:54 -0500

    cards section skills

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CvComponent } from './pages/cv/cv.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BabyShowerComponent } from './pages/baby-shower/baby-shower.component';
+import { SkillsComponent } from './pages/home/components/skills/skills.component';
+import { ProjectsComponent } from './pages/home/components/projects/projects.component';
 
 const routes: Routes = [
   {path: "Porfolio-Oscar-Giraldo", component: HomeComponent},
+  /* ####### navigation menu ####### */
+  {path: "skills", component: SkillsComponent},
+  {path: "projects", component: ProjectsComponent},
   {path: "Curriculum-Vitae", component: CvComponent},
   {path: "BabyShower-Card", component: BabyShowerComponent},
   {path: "", redirectTo: "/Porfolio-Oscar-Giraldo", pathMatch: "full"},
